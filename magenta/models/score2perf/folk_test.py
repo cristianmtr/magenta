e6f71131dfcde366d7429b241b8aea92b6472455
cristian <cristi.mtr@gmail.com>
2019-03-11 10:34:45 +0100

    using API

--- magenta/models/score2perf/folk_test.py
+++ magenta/models/score2perf/folk_test.py
@@ -6,21 +6,37 @@ import pypianoroll
 
 from music_encoders import MidiPerformanceEncoder
 
-encoder = MidiPerformanceEncoder(100, 32, 40, 99)
+encoder = MidiPerformanceEncoder(100, 1, 40, 99)
 
-fnames = glob.glob("D:\data\folkdataset\*.mid")[:10]
+fnames = glob.glob("D:\\data\\folkdataset\\*.mid")[:2]
 for fname in fnames:
     seq = encoder.encode(fname)
 
     pianoroll_original = pypianoroll.Multitrack(fname).tracks[0].pianoroll
-
-    # print(seq)
-
     mid = encoder.decode(seq)
     # print(mid)
-    pianoroll_encoded = pypianoroll.Multitrack(fname).tracks[0].pianoroll
+    pianoroll_encoded = pypianoroll.Multitrack(mid).tracks[0].pianoroll
 
     equality = pianoroll_encoded == pianoroll_original
-    assert False not in equality
+    print(fname)
+    print(mid)
+
+
+# test padding effect
+for i, fname in enumerate(fnames):
+    pianoroll_original = pypianoroll.Multitrack(fname).tracks[0].pianoroll
+
+    seq = encoder.encode(fname)
+    for _ in range(50):
+        seq.append(0)
+
+    mid = encoder.decode(seq, strip_extraneous=True)
+    pianoroll_encoded = pypianoroll.Multitrack(mid).tracks[0].pianoroll
+
+    equality = pianoroll_encoded == pianoroll_original
+    print(fname)
+    print(mid)
+
+
 
 print('ALL GOOD')
