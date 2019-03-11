2971bb947952e4a23187ea044c366927c67bb16a
cristian <cristi.mtr@gmail.com>
2019-03-11 10:52:46 +0100

    have one encoder

--- magenta/models/score2perf/folk_test.py
+++ magenta/models/score2perf/folk_test.py
@@ -6,37 +6,38 @@ import pypianoroll
 
 from music_encoders import MidiPerformanceEncoder
 
-encoder = MidiPerformanceEncoder(100, 1, 40, 99)
+encoder = MidiPerformanceEncoder(100, 0, 40, 99)
 
-fnames = glob.glob("D:\\data\\folkdataset\\*.mid")[:2]
-for fname in fnames:
-    seq = encoder.encode(fname)
+if __name__ == "__main__":
+    fnames = glob.glob("D:\\data\\folkdataset\\*.mid")[:2]
+    for fname in fnames:
+        seq = encoder.encode(fname)
 
-    pianoroll_original = pypianoroll.Multitrack(fname).tracks[0].pianoroll
-    mid = encoder.decode(seq)
-    # print(mid)
-    pianoroll_encoded = pypianoroll.Multitrack(mid).tracks[0].pianoroll
+        pianoroll_original = pypianoroll.Multitrack(fname).tracks[0].pianoroll
+        mid = encoder.decode(seq)
+        # print(mid)
+        pianoroll_encoded = pypianoroll.Multitrack(mid).tracks[0].pianoroll
 
-    equality = pianoroll_encoded == pianoroll_original
-    print(fname)
-    print(mid)
-
-
-# test padding effect
-for i, fname in enumerate(fnames):
-    pianoroll_original = pypianoroll.Multitrack(fname).tracks[0].pianoroll
-
-    seq = encoder.encode(fname)
-    for _ in range(50):
-        seq.append(0)
-
-    mid = encoder.decode(seq, strip_extraneous=True)
-    pianoroll_encoded = pypianoroll.Multitrack(mid).tracks[0].pianoroll
-
-    equality = pianoroll_encoded == pianoroll_original
-    print(fname)
-    print(mid)
+        equality = pianoroll_encoded == pianoroll_original
+        print(fname)
+        print(mid)
 
 
+    # test padding effect
+    for i, fname in enumerate(fnames):
+        pianoroll_original = pypianoroll.Multitrack(fname).tracks[0].pianoroll
 
-print('ALL GOOD')
+        seq = encoder.encode(fname)
+        for _ in range(50):
+            seq.append(0)
+
+        mid = encoder.decode(seq, strip_extraneous=True)
+        pianoroll_encoded = pypianoroll.Multitrack(mid).tracks[0].pianoroll
+
+        equality = pianoroll_encoded == pianoroll_original
+        print(fname)
+        print(mid)
+
+
+
+    print('ALL GOOD')
