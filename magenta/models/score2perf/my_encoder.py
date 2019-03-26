ac2d8ae455fdd07f4b46dec82aedab22fcb6bbbd
cristian <cristi.mtr@gmail.com>
2019-03-26 11:41:49 +0100

    add function to print events

--- magenta/models/score2perf/my_encoder.py
+++ magenta/models/score2perf/my_encoder.py
@@ -2,3 +2,20 @@ from music_encoders import MidiPerformanceEncoder
 
 # min and max from a piano range, see http://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
 encoder = MidiPerformanceEncoder(100, 0, 21, 108)
+
+
+def printevents(l_events):
+    results = []
+    for e in l_events:
+        # my_encoder.encoder._encoding.decode_event(131)
+        decoded = encoder._encoding.decode_event(e)
+        event_type_str = None
+        if decoded.event_type == decoded.NOTE_ON:
+            event_type_str = "Note ON"
+        elif decoded.event_type == decoded.NOTE_OFF:
+            event_type_str = "Note OFF"
+        elif decoded.event_type == decoded.TIME_SHIFT:
+            event_type_str = "Time Shift"
+        results.append('PerformanceEvent(%r, %r)' %
+                       (event_type_str, decoded.event_value))
+    return results
